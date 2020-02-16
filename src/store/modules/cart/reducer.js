9ff89f0ce00d4a2c7c30be41d6214ce254eb1765
Matheus Filipe <mfcr031@gmail.com>
2020-02-16 11:34:45 -0300

    listando no carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,8 +1,18 @@
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return [
+        ...state,
+        {
+          ...action.product,
+          amount: 1,
+        },
+      ];
     default:
       return state;
   }
 }
+
+/**
+ * obs.: podemos adicionar informações a mais ao estado
+ */
