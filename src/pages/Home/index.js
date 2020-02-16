075046386a945ab09d7563f9b9081a09ea49edda
Matheus Filipe <mfcr031@gmail.com>
2020-02-16 12:52:33 -0300

    refatorando as actions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 // vai conectar o componente com o estado do redux
 import { connect } from 'react-redux';
-
+import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
@@ -27,13 +29,10 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props;
+    const { addToCart } = this.props;
 
     // disparar uma action ao redux
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    });
+    addToCart(product);
   };
 
   render() {
@@ -65,4 +64,7 @@ class Home extends Component {
   }
 }
 
-export default connect()(Home);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(Home);
